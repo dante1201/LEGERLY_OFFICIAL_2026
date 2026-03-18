c37bb12528ec7505c4889f6ff4ff52dda3a261ef
dante1201 <34242199+dante1201@users.noreply.github.com>
2026-03-18 12:32:15 -0400

    Update server to use Render port

--- L_Backend/server.js
+++ L_Backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
@@ -24,8 +25,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
-    );
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
