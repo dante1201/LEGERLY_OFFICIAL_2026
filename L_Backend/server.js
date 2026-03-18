37ede813f707eb7a653df2af067f1690e0684f47
dante1201 <34242199+dante1201@users.noreply.github.com>
2026-03-18 10:35:08 -0400

    Update server to use Render port

--- L_Backend/server.js
+++ L_Backend/server.js
@@ -4,20 +4,28 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const expenseRoutes = require("./Routes/expenseRoutes");
 const employeeRoutes = require("./Routes/employeeRoutes");
+
 app.use("/employees", employeeRoutes);
 app.use("/expenses", expenseRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   })
   .catch((err) => console.log(err));
